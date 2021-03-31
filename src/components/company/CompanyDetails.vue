ecdabaabac903f3083913769f49d1827d9ac8dd9
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-03-31 12:22:54 +0200

    fetch a company detail

--- src/components/company/CompanyDetails.vue
+++ src/components/company/CompanyDetails.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h1>Company details</h1>
-    <div>
-      {{ company.denomination.value }}
-    </div>
+    <div>siren: {{ company.siren.value }}</div>
+    <div>denomination: {{ company.denomination.value }}</div>
+    <div>ape: {{ company.ape.value }}</div>
+    <div>postal_code: {{ company.postal_code.value }}</div>
+    <div>town: {{ company.town.value }}</div>
+    <div>devise: {{ company.devise.value }}</div>
   </div>
 </template>
 
---
+++ src/services/companies/CompaniesService.fake.js
@@ -0,0 +1,591 @@
+const stubResponse = {
+  siren: {
+    value: "322120916",
+    description: "SIREN",
+  },
+  denomination: {
+    value: "APPLE FRANCE",
+    description: "Dénomination",
+  },
+  ape: {
+    value: "Activités des agences de publicité",
+    description: "Code Activité Principale Exercée (NAF)",
+  },
+  postal_code: {
+    value: "75116",
+    description: "Code Postal",
+  },
+  town: {
+    value: "PARIS",
+    description: "Commune",
+  },
+  devise: {
+    value: "Euro",
+    description: "Devise",
+  },
+  declarations: [
+    {
+      declaration: {
+        value: 2016,
+        description: "Année de déclaration",
+      },
+      financial_data: [
+        {
+          FR: {
+            account: "Compte annuel complet",
+            value: 81132600,
+            description: "Total des produits d’exploitation",
+          },
+        },
+        {
+          FW: {
+            account: "Compte annuel complet",
+            value: 7989280,
+            description: "Autres achats et charges externes",
+          },
+        },
+        {
+          FX: {
+            account: "Compte annuel complet",
+            value: 1731200,
+            description: "Impôts, taxes et versements assimilés",
+          },
+        },
+        {
+          FY: {
+            account: "Compte annuel complet",
+            value: 17116100,
+            description: "Salaires et traitements",
+          },
+        },
+        {
+          FZ: {
+            account: "Compte annuel complet",
+            value: 10456900,
+            description: "Charges sociales",
+          },
+        },
+        {
+          GE: {
+            account: "Compte annuel complet",
+            value: 14243,
+            description: "Autres charges",
+          },
+        },
+        {
+          GF: {
+            account: "Compte annuel complet",
+            value: 37998500,
+            description: "Total des charges d’exploitation",
+          },
+        },
+        {
+          GG: {
+            account: "Compte annuel complet",
+            value: 43134200,
+            description: "Résultat d'exploitation",
+          },
+        },
+        {
+          GK: {
+            account: "Compte annuel complet",
+            value: 249012,
+            description:
+              "Produits des autres valeurs mobilières et créances de l’actif immobilisé",
+          },
+        },
+        {
+          GN: {
+            account: "Compte annuel complet",
+            value: 26004,
+            description: "Différences positives de change",
+          },
+        },
+        {
+          GP: {
+            account: "Compte annuel complet",
+            value: 275015,
+            description: "Total des produits financiers",
+          },
+        },
+        {
+          GS: {
+            account: "Compte annuel complet",
+            value: 495,
+            description: "Différences négatives de change",
+          },
+        },
+        {
+          GU: {
+            account: "Compte annuel complet",
+            value: 495,
+            description: "Total des charges financières",
+          },
+        },
+        {
+          GV: {
+            account: "Compte annuel complet",
+            value: 274520,
+            description: "Résultat financier",
+          },
+        },
+        {
+          GW: {
+            account: "Compte annuel complet",
+            value: 43408700,
+            description: "Résultat en cours avant impôts",
+          },
+        },
+        {
+          HE: {
+            account: "Compte annuel complet",
+            value: -24680,
+            description: "Charges exceptionnelles sur opérations de gestion",
+          },
+        },
+        {
+          HF: {
+            account: "Compte annuel complet",
+            value: 535,
+            description: "Charges exceptionnelles sur opérations en capital",
+          },
+        },
+        {
+          HH: {
+            account: "Compte annuel complet",
+            value: -24145,
+            description: "Total des charges exceptionnelles",
+          },
+        },
+        {
+          HI: {
+            account: "Compte annuel complet",
+            value: 24145,
+            description: "Résultat exceptionnel",
+          },
+        },
+        {
+          HJ: {
+            account: "Compte annuel complet",
+            value: 2849880,
+            description:
+              "Participation des salariés aux résultats de l’entreprise",
+          },
+        },
+        {
+          HK: {
+            account: "Compte annuel complet",
+            value: 14628700,
+            description: "Impôts sur les bénéfices",
+          },
+        },
+        {
+          HL: {
+            account: "Compte annuel complet",
+            value: 81407700,
+            description: "Total des produits",
+          },
+        },
+        {
+          HM: {
+            account: "Compte annuel complet",
+            value: 55453400,
+            description: "Total des charges",
+          },
+        },
+        {
+          FM: {
+            account: "Compte annuel complet",
+            value: 81133600,
+            description: "Production stockée",
+          },
+        },
+        {
+          FQ: {
+            account: "Compte annuel complet",
+            value: -908,
+            description: "Autres produits",
+          },
+        },
+      ],
+    },
+    {
+      declaration: {
+        value: 2017,
+        description: "Année de déclaration",
+      },
+      financial_data: [
+        {
+          FR: {
+            account: "Compte annuel complet",
+            value: 89369000,
+            description: "Total des produits d’exploitation",
+          },
+        },
+        {
+          FW: {
+            account: "Compte annuel complet",
+            value: 8267970,
+            description: "Autres achats et charges externes",
+          },
+        },
+        {
+          FX: {
+            account: "Compte annuel complet",
+            value: 2173720,
+            description: "Impôts, taxes et versements assimilés",
+          },
+        },
+        {
+          FY: {
+            account: "Compte annuel complet",
+            value: 19886700,
+            description: "Salaires et traitements",
+          },
+        },
+        {
+          FZ: {
+            account: "Compte annuel complet",
+            value: 13433900,
+            description: "Charges sociales",
+          },
+        },
+        {
+          GE: {
+            account: "Compte annuel complet",
+            value: 324,
+            description: "Autres charges",
+          },
+        },
+        {
+          GF: {
+            account: "Compte annuel complet",
+            value: 44662700,
+            description: "Total des charges d’exploitation",
+          },
+        },
+        {
+          GG: {
+            account: "Compte annuel complet",
+            value: 44706300,
+            description: "Résultat d'exploitation",
+          },
+        },
+        {
+          GK: {
+            account: "Compte annuel complet",
+            value: 419305,
+            description:
+              "Produits des autres valeurs mobilières et créances de l’actif immobilisé",
+          },
+        },
+        {
+          GP: {
+            account: "Compte annuel complet",
+            value: 419305,
+            description: "Total des produits financiers",
+          },
+        },
+        {
+          GS: {
+            account: "Compte annuel complet",
+            value: -12763,
+            description: "Différences négatives de change",
+          },
+        },
+        {
+          GU: {
+            account: "Compte annuel complet",
+            value: -12763,
+            description: "Total des charges financières",
+          },
+        },
+        {
+          GV: {
+            account: "Compte annuel complet",
+            value: 432069,
+            description: "Résultat financier",
+          },
+        },
+        {
+          FG: {
+            account: "Compte annuel complet",
+            value: 89360000,
+            description: "Production vendue services",
+          },
+        },
+        {
+          HE: {
+            account: "Compte annuel complet",
+            value: 44235,
+            description: "Charges exceptionnelles sur opérations de gestion",
+          },
+        },
+        {
+          HH: {
+            account: "Compte annuel complet",
+            value: 44235,
+            description: "Total des charges exceptionnelles",
+          },
+        },
+        {
+          HI: {
+            account: "Compte annuel complet",
+            value: -44235,
+            description: "Résultat exceptionnel",
+          },
+        },
+        {
+          HJ: {
+            account: "Compte annuel complet",
+            value: 2562330,
+            description:
+              "Participation des salariés aux résultats de l’entreprise",
+          },
+        },
+        {
+          FJ: {
+            account: "Compte annuel complet",
+            value: 89360000,
+            description: "Chiffres d’affaires nets",
+          },
+        },
+        {
+          HL: {
+            account: "Compte annuel complet",
+            value: 89788300,
+            description: "Total des produits",
+          },
+        },
+        {
+          HM: {
+            account: "Compte annuel complet",
+            value: 61726700,
+            description: "Total des charges",
+          },
+        },
+        {
+          HN: {
+            account: "Compte annuel complet",
+            value: 28061700,
+            description:
+              "Bénéfices ou perte (Total des produits ‐ Total des charges)",
+          },
+        },
+        {
+          YP: {
+            account: "Compte annuel complet",
+            value: 181,
+            description: "Effectif moyen du personnel",
+          },
+        },
+        {
+          FQ: {
+            account: "Compte annuel complet",
+            value: 9009,
+            description: "Autres produits",
+          },
+        },
+      ],
+    },
+    {
+      declaration: {
+        value: 2018,
+        description: "Année de déclaration",
+      },
+      financial_data: [
+        {
+          FR: {
+            account: "Compte annuel complet",
+            value: 401394000,
+            description: "Total des produits d’exploitation",
+          },
+        },
+        {
+          FW: {
+            account: "Compte annuel complet",
+            value: 14798200,
+            description: "Autres achats et charges externes",
+          },
+        },
+        {
+          FX: {
+            account: "Compte annuel complet",
+            value: 7105440,
+            description: "Impôts, taxes et versements assimilés",
+          },
+        },
+        {
+          FY: {
+            account: "Compte annuel complet",
+            value: 41347300,
+            description: "Salaires et traitements",
+          },
+        },
+        {
+          FZ: {
+            account: "Compte annuel complet",
+            value: 27459700,
+            description: "Charges sociales",
+          },
+        },
+        {
+          GE: {
+            account: "Compte annuel complet",
+            value: 854,
+            description: "Autres charges",
+          },
+        },
+        {
+          GF: {
+            account: "Compte annuel complet",
+            value: 92770300,
+            description: "Total des charges d’exploitation",
+          },
+        },
+        {
+          GG: {
+            account: "Compte annuel complet",
+            value: 308623000,
+            description: "Résultat d'exploitation",
+          },
+        },
+        {
+          GL: {
+            account: "Compte annuel complet",
+            value: 1120320,
+            description: "Autres intérêts et produits assimilés",
+          },
+        },
+        {
+          GN: {
+            account: "Compte annuel complet",
+            value: 8980,
+            description: "Différences positives de change",
+          },
+        },
+        {
+          FG: {
+            account: "Compte annuel complet",
+            value: 399474000,
+            description: "Production vendue services",
+          },
+        },
+        {
+          GV: {
+            account: "Compte annuel complet",
+            value: 1129300,
+            description: "Résultat financier",
+          },
+        },
+        {
+          GW: {
+            account: "Compte annuel complet",
+            value: 309753000,
+            description: "Résultat en cours avant impôts",
+          },
+        },
+        {
+          HA: {
+            account: "Compte annuel complet",
+            value: 2511850000,
+            description: "Produits exceptionnels sur opérations de gestion",
+          },
+        },
+        {
+          FJ: {
+            account: "Compte annuel complet",
+            value: 399474000,
+            description: "Chiffres d’affaires nets",
+          },
+        },
+        {
+          HE: {
+            account: "Compte annuel complet",
+            value: 2356,
+            description: "Charges exceptionnelles sur opérations de gestion",
+          },
+        },
+        {
+          HG: {
+            account: "Compte annuel complet",
+            value: 1152190000,
+            description:
+              "Dotations exceptionnelles aux amortissements et provisions",
+          },
+        },
+        {
+          HH: {
+            account: "Compte annuel complet",
+            value: 1152200000,
+            description: "Total des charges exceptionnelles",
+          },
+        },
+        {
+          HI: {
+            account: "Compte annuel complet",
+            value: 1359650000,
+            description: "Résultat exceptionnel",
+          },
+        },
+        {
+          HJ: {
+            account: "Compte annuel complet",
+            value: 6251400,
+            description:
+              "Participation des salariés aux résultats de l’entreprise",
+          },
+        },
+        {
+          HK: {
+            account: "Compte annuel complet",
+            value: 92828000,
+            description: "Impôts sur les bénéfices",
+          },
+        },
+        {
+          HL: {
+            account: "Compte annuel complet",
+            value: 2914370000,
+            description: "Total des produits",
+          },
+        },
+        {
+          HM: {
+            account: "Compte annuel complet",
+            value: 1344050000,
+            description: "Total des charges",
+          },
+        },
+        {
+          HN: {
+            account: "Compte annuel complet",
+            value: 1570330000,
+            description:
+              "Bénéfices ou perte (Total des produits ‐ Total des charges)",
+          },
+        },
+        {
+          FP: {
+            account: "Compte annuel complet",
+            value: 1920000,
+            description:
+              "Reprises sur amortissements et provisions, transfert de charges",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const returnStubIn2s = () => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(stubResponse);
+    }, 2000)
+  );
+};
+
+export default {
+  getCompaniesFromSiren: () => returnStubIn2s(),
+};
