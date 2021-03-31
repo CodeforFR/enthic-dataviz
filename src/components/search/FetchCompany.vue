65503379a596cefa126926cf90573f663ceb0f91
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-03-31 12:22:54 +0200

    fetch a company detail

--- src/components/search/FetchCompany.vue
+++ src/components/search/FetchCompany.vue
@@ -1,11 +1,74 @@
 <template>
-  <div>Get Company with siren {{ siren }}</div>
+  <div>
+    <img
+      v-if="loading"
+      class="loading-indicator"
+      src="@/assets/loading.svg"
+      alt=""
+    />
+    <h3 v-if="loading">
+      recherche en cours de l'entreprise avec le SIREN '{{ siren }}'
+    </h3>
+    <div class="error-message" v-if="error">
+      Erreur lors de la récuperation des donnés : {{ error }}
+    </div>
+    <div v-if="results">
+      <CompanyDetails :company="results" />
+    </div>
+    <div v-if="noResult">
+      Il n'y a pas d'entreprise avec le SIREN '{{ siren }}' dans les données
+      Enthic.
+    </div>
+  </div>
 </template>
 
 <script>
+import CompanyDetails from "@/components/company/CompanyDetails.vue";
+import CompaniesService from "@/services/companies/CompaniesService";
+// import CompaniesService from "@/services/companies/CompaniesService.fake";
+
 export default {
   props: ["siren"],
+  components: {
+    CompanyDetails,
+  },
+  data() {
+    return {
+      results: null,
+      loading: true,
+      error: null,
+      noResult: false,
+    };
+  },
+  created() {
+    this.search(() => CompaniesService.getCompaniesFromSiren(this.siren));
+  },
+  methods: {
+    async search(fetchOperation) {
+      console.log("search");
+      try {
+        this.loading = true;
+        this.results = null;
+        const results = await fetchOperation();
+        this.error = null;
+        this.results = results;
+        this.noResult = results === null;
+      } catch (e) {
+        this.error = e;
+        this.noResult = false;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.loading-indicator {
+  transform: scale(0.4);
+}
+.error-message {
+  color: red;
+}
+</style>
