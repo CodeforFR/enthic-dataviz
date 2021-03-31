024fac8c10d0167fa7ac6bf94c60a35856f7d3b5
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-03-31 10:14:28 +0200

    search sekeleton

---
+++ src/components/company/CompanyDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Company details</h1>
+    <div>
+      {{ company.denomination.value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["company"],
+};
+</script>
+
+<style></style>
