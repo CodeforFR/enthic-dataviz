92ea265fa2025a043a50e12a2401aa31cc2d1c26
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-04-01 08:28:47 +0200

    refactor addAccountingCalculations

--- src/services/companyData/CompanyDataService.js
+++ src/services/companyData/CompanyDataService.js
@@ -4,7 +4,7 @@ import AccountingService from "@/services/accounting/AccountingService";
 const getCompanyDataBySiren = async (siren) => {
   const companyData1 = await CompaniesRepository.getCompanyDataBySiren(siren);
   console.log("companyData1-a", companyData1);
-  const companyData2 = AccountingService.accountingTransformation1(
+  const companyData2 = AccountingService.addAccountingCalculations(
     companyData1
   );
   console.log("companyData2-b", companyData2);
