84c6f54b7d55650de6ace53185f8b472d984e93a
Grimbsi <quentin.madura@gmail.com>
2021-04-12 15:24:55 +0200

    [+] Handle responsiveness, clean useless classes

--- src/components/home/PartnersSection.vue
+++ src/components/home/PartnersSection.vue
@@ -3,50 +3,52 @@
     <div class="columns is-centered">
       <div class="column is-10 content">
         <h2 class="has-text-centered">L'écosystème</h2>
-        <hr />
         <div class="columns is-centered is-multiline is-vcentered">
-          <div class="column is-3 content is-horizontal-center">
-            <figure class="image img-mini is-inline-block">
+          <div class="column is-3 is-mobile-center">
+            <figure class="image is-128">
               <a href="https://www.enthic.fr"
-                ><img src="/favicon.ico" alt="logo Enthic" style="width: 40%"
+                ><img src="/favicon.ico" alt="logo Enthic" class="is-128"
               /></a>
             </figure>
           </div>
-
-          <div class="column is-3 content is-horizontal-center">
-            <figure class="image img-mini is-inline-block">
+          <div class="column is-3 is-mobile-center">
+            <figure class="image is-128">
               <a href="https://codefor.fr/"
                 ><img
                   src="https://fork.osp.cat/uploads/decidim/organization/logo/1/medium_logo-code-for-france-alpha-gray.png"
                   alt="logo Code For France"
+                  class="is-128"
               /></a>
             </figure>
           </div>
 
-          <div class="column is-3 content is-horizontal-center">
-            <figure class="image img-mini is-inline-block">
+          <div class="column is-3 is-mobile-center">
+            <figure class="image is-128">
               <a href="https://wexample.com/"
                 ><img
                   src="https://wexample.com/wp-content/uploads/2019/08/header-site-web-v3.png"
                   alt="Logo Wexample"
+                  class="is-128"
               /></a>
             </figure>
           </div>
-          <div class="column is-3 content is-horizontal-center">
-            <figure class="image img-mini is-inline-block">
+          <div class="column is-3 is-mobile-center">
+            <figure class="image is-128">
               <a href="https://dataforgood.fr"
                 ><img
                   src="https://dataforgood.fr/img/logo-white.png"
+                  class="is-128"
                   alt="logo Data For Good"
               /></a>
             </figure>
           </div>
-          <div class="column is-3 content is-horizontal-center">
-            <figure class="image img-mini is-inline-block">
+          <div class="column is-3 is-mobile-center">
+            <figure class="image is-128">
               <a href="https://www.latitudes.cc/"
                 ><img
                   src="//user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/1127947/550388_944467.png"
                   alt="logo Latitude - Exploring Tech for Good"
+                  class="is-128"
               /></a>
             </figure>
           </div>
@@ -57,6 +59,9 @@
 </template>
 
 <style lang="scss" scoped>
+.is-128 {
+  width: 128px;
+}
 .logo-list {
   display: flex;
   flex-direction: column;
@@ -67,7 +72,12 @@
     margin: 1rem;
   }
 }
-
+@media (max-width: 960px) {
+  .is-mobile-center {
+    justify-content: center;
+    display: flex;
+  }
+}
 @media (min-width: 960px) {
   .logo-list {
     flex-direction: row;
