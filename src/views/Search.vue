822511da56080c5d443381b35a0c319800ca7294
Grimbsi <quentin.madura@gmail.com>
2021-04-20 10:30:13 +0200

    [+] SearchBar <- adding text in the search bar

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <SearchBar />
+    <SearchBar :searchText="text" />
     <SearchResults :text="text" />
   </div>
 </template>
@@ -12,7 +12,7 @@ import SearchResults from "../components/search/SearchResults.vue";
 export default {
   components: {
     SearchResults,
-    SearchBar
+    SearchBar,
   },
   computed: {
     text() {
