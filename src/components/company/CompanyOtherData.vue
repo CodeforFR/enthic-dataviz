dea8912fc1a6db182ca1c81526d9951a70143a05
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-05-25 17:25:01 +0200

    margins other data

--- src/components/company/CompanyOtherData.vue
+++ src/components/company/CompanyOtherData.vue
@@ -1,8 +1,14 @@
 <template>
   <Widget title="Autres données" titleColor="#ff99ff" v-if="companyData">
-    <div v-for="(year, index) in companyData.yearData" :key="index">
-      {{ year.year }}
-      <ul>
+    <div
+      class="m-l-30 m-b-20"
+      v-for="(year, index) in companyData.yearData"
+      :key="index"
+    >
+      <span class="title is-6">
+        {{ year.year }}
+      </span>
+      <ul class="m-l-30">
         <li v-for="(data, index) in year.data" :key="index">
           {{ data.description }} : {{ data.value }}
         </li>
