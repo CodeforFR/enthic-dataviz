a88edd57c898038f9fab2da5578b618d1bfcafca
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-05-25 10:30:02 +0200

    prepare extraction

---
+++ src/components/company/CompanyNoCompteDeResultats.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="columns" v-if="!companyData.comptesDeResultats">
+    <div class="column">
+      <div class="box has-background-info">
+        <h3 class="title is-12">
+          Aucun compte disponible pour cette entreprise
+        </h3>
+        <p>
+          Peut-être pour des raisons de confidentialité ou de problème dans la
+          saisie
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompanyNoCompteDeResultats",
+  props: ["companyData"],
+};
+</script>
