bd27599fc7e3e3e779e2651710737f56f338d080
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-05-25 16:39:03 +0200

    widget test

---
+++ src/components/company/SampleWidget.vue
@@ -0,0 +1,19 @@
+<template>
+  <Widget title="My sample Title" titleColor="#9999ff">
+    <div>My personal Content</div>
+  </Widget>
+</template>
+
+<script>
+import Widget from "./Widget.vue";
+
+export default {
+  components: { Widget },
+};
+</script>
+
+<style>
+.fuck {
+  background-color: red;
+}
+</style>
---
+++ src/components/company/Widget.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="box widget-box">
+        <h3 class="title is-3 widget-title" :style="titleStyle">{{ title }}</h3>
+        <div class="widget-content">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Widget",
+  props: {
+    title: String,
+    titleColor: String,
+  },
+  computed: {
+    titleStyle() {
+      return { backgroundColor: this.titleColor };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.widget-box {
+  padding: 0;
+}
+
+.widget-title {
+  padding: 0.75rem;
+  margin-bottom: 0.5rem;
+  border-radius: 6px 6px 30px 30px;
+}
+
+.widget-content {
+  padding: 0.75rem;
+}
+</style>
