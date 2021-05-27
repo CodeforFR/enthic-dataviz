646584b700d9a1a56d0772678d306e177404e9e4
Léonard Michelet <leonard.michelet@smile.fr>
2021-05-27 10:01:30 +0200

    use enthic test server, instead of prod one

--- src/repositories/company/CompanyRepository.js
+++ src/repositories/company/CompanyRepository.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://api.enthic.fr",
+  baseURL: "http://test.enthic.fr:5432",
   withCredentials: false,
   headers: {
     Accept: "application/json",
