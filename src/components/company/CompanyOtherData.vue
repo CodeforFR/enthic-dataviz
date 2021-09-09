6fccaf5de1c9ec128a36357662da3e531878a708
Léonard Michelet <leonard.michelet@smile.fr>
2021-09-09 15:19:40 +0200

    remove obsolete code

--- src/components/company/CompanyOtherData.vue
+++ src/components/company/CompanyOtherData.vue
@@ -14,124 +14,6 @@
         </li>
       </ul>
     </div>
-    <div class="description">
-      <!-- BLOCK MAIN TAGS -->
-      <div id="block-main-tags" v-if="isPositionFilled('block_main_tags')">
-        <span
-          v-if="getCustomBlockTitle('block_main_tags')"
-          class="has-text-weight-semibold has-text-primary has-text-primary-c"
-        >
-          {{ getCustomBlockTitle("block_main_tags") }}
-        </span>
-        <br />
-        <span
-          class="tag"
-          v-for="(tag, i) in matchProjectWithConfig('block_main_tags')"
-          :key="tag + i"
-        >
-          {{ tag }}
-        </span>
-        <br /><br />
-      </div>
-
-      <!-- BLOCK ADDRESS -->
-      <p id="block-address" v-if="isPositionFilled('block_address')">
-        <span class="icon">
-          <img class="image is-16x16" src="@/assets/icons/icon_pin.svg" />
-        </span>
-        {{ matchProjectWithConfig("block_address") }}
-      </p>
-
-      <!-- BLOCK PRE ABSTRACT -->
-      <p id="block-pre-abstract" v-if="isPositionFilled('block_pre_abstract')">
-        <span
-          v-if="getCustomBlockTitle('block_pre_abstract')"
-          class="has-text-weight-semibold has-text-primary has-text-primary-c"
-        >
-          {{ getCustomBlockTitle("block_pre_abstract") }}
-          <br /><br />
-        </span>
-        {{ matchProjectWithConfig("block_pre_abstract") }}
-      </p>
-
-      <!-- BLOCK PARTNERS -->
-      <div id="block-partners" v-if="isPositionFilled('block_partners')">
-        <p>{{ matchProjectWithConfig("block_partners") }}</p>
-      </div>
-
-      <!-- BLOCK POST ABSTRACT 1 -->
-      <p id="block-post-abstract-1"></p>
-
-      <!-- BLOCK POST ABSTRACT 2 -->
-      <p id="block-post-abstract-2"></p>
-
-      <!-- BLOCK POST ABSTRACT 3 -->
-      <p
-        id="block-post-abstract-3"
-        v-if="isPositionFilled('block_post_abstract_3')"
-      >
-        <span
-          v-if="getCustomBlockTitle('block_post_abstract_3')"
-          class="has-text-weight-semibold has-text-primary has-text-primary-c"
-        >
-          {{ getCustomBlockTitle("block_post_abstract_3") }}
-          <br /><br />
-        </span>
-        {{ matchProjectWithConfig("block_post_abstract_3") }}
-      </p>
-    </div>
-
-    <!-- BLOCK SERVICES -->
-    <div
-      class="added"
-      id="block-services"
-      v-if="isPositionFilled('block_services')"
-    >
-      <div class="columns">
-        <div class="column is-12">
-          <div>
-            <span
-              class="
-                has-text-weight-semibold has-text-primary has-text-primary-c
-              "
-            >
-              <!-- {{ servicesData }}  -->
-              {{ getDefaultText("services") }}
-              : <br /><br />
-            </span>
-            <span>
-              {{ matchProjectWithConfig("block_services") }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- BLOCK BOTTOM 2 -->
-    <div
-      class="added"
-      id="block-LB2"
-      v-if="isPositionFilled('block_left_bottom_2')"
-    >
-      <div class="columns">
-        <div class="column is-12">
-          <div>
-            <span
-              v-if="getCustomBlockTitle('block_left_bottom_2')"
-              class="
-                has-text-weight-semibold has-text-primary has-text-primary-c
-              "
-            >
-              {{ getCustomBlockTitle("block_left_bottom_2") }}
-              <br /><br />
-            </span>
-            <span>
-              {{ matchProjectWithConfig("block_left_bottom_2") }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
   </Widget>
 </template>
 
@@ -141,43 +23,8 @@ import Widget from "./Widget.vue";
 export default {
   name: "CompanyOtherData",
   props: ["companyData"],
-  data() {
-    return {
-      contentFields: [
-        {
-          custom_title: null,
-          field: "Name",
-          field_format: { retrieve: [0], trim: null, type: "object" },
-          is_visible: true,
-          locale: "fr",
-          position: "block_title",
-        },
-      ],
-    };
-  },
   components: {
     Widget,
-  },
-  methods: {
-    isPositionFilled(fieldBlock) {
-      console.log("isPositionFilled", fieldBlock);
-      // console.log("isPositionFilled /  fieldBlock :", fieldBlock)
-      return false; // this.listOfPositions.indexOf(fieldBlock) !== -1;
-    },
-    getCustomBlockTitle(fieldBlock) {
-      let customBlockTitle = undefined;
-      const contentField = this.contentFields.find(
-        (f) => f.position === fieldBlock
-      );
-      if (contentField) {
-        customBlockTitle = contentField.custom_title;
-      }
-      return customBlockTitle;
-    },
-    matchProjectWithConfig(fieldBlock) {
-      console.log("matchProjectWithConfig fieldBlock =", fieldBlock);
-      return this.noData;
-    },
   },
 };
 </script>
@@ -188,21 +35,5 @@ export default {
 
 ul {
   list-style-type: circle;
-}
-
-.description,
-.added {
-  // background-color: $app-blue-deep;
-  background-color: white;
-  // color: white;
-  padding: 1em;
-  margin-bottom: 1em;
-}
-
-.tag {
-  background-color: $app-secondary;
-  color: white;
-  margin-right: 1em;
-  margin-bottom: 0.5em;
 }
 </style>
