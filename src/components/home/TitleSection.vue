6ecfd3c005ad04b824fdced230f84975bd0ff249
Léonard Michelet <leonard.michelet@smile.fr>
2021-09-09 15:15:52 +0200

    split home page into several files, one for each part

--- src/components/home/TitleSection.vue
+++ src/components/home/TitleSection.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="layout-header">
-    <h1 class="is-size-1">Enthic DataViz</h1>
-    <h2 class="is-size-2">
-      Explorez les données financières des entreprises françaises
-    </h2>
+  <div class="columns p-20 m-b-30 m-t-30 has-background-white-bis">
+    <div class="column">
+      <div class="content has-text-centered">
+        <h2 class="is-size-2">
+          Explorez les données financières des entreprises françaises!
+        </h2>
+        <SearchBar />
+        <div class="m-t-20">
+          par exemple l'entreprise
+          <router-link class="result-link" to="/recherche?text=enercoop">
+            "Enercoop"</router-link
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-<style lang="scss" scoped>
-.layout-header {
-  text-align: center;
-  margin: 1rem;
-}
 
-.wip {
-  color: burlywood;
-}
-</style>
+<script>
+import SearchBar from "@/components/search/SearchBar";
+
+export default {
+  name: "TitleSection",
+  components: {
+    SearchBar,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,7 @@
 <template>
   <div class="container is-fullheight m-t-30">
-    <div class="columns p-20 m-b-30 m-t-30 has-background-white-bis">
-      <div class="column">
-        <div class="content has-text-centered">
-          <h2 class="is-size-2">
-            Explorez les données financières des entreprises françaises!
-          </h2>
-          <SearchBar />
-          <div class="m-t-20">
-            par exemple l'entreprise
-            <router-link class="result-link" to="/recherche?text=enercoop">
-              "Enercoop"</router-link
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="columns p-20 m-b-10 has-background-white-bis is-vcentered">
-      <div class="column is-half">
-        <div class="content">
-          <h1 class="is-size-1">Enthic Dataviz : c'est quoi?</h1>
-          <p>
-            Enthic DataViz est un outil de data visualisation de la base de
-            données <a href="https://www.enthic.fr">Enthic</a> qui contient des
-            données sur plus de 1,6 millions d'entreprises françaises :
-            bénéfice, chiffre d'affaires, salaire, impôt, et plein d'autres
-            informations que seuls les comptables comprennent (tous les détails
-            d'un compte de résultat en fait).
-          </p>
-          <p>
-            Le but est de rendre les données compréhensible au plus grand
-            nombre, à l'aide de graphiques, d'indicateurs, de comparaisons et
-            d'explications.
-          </p>
-          <p>
-            Nous avons monté ce projet car nous pensons que ces données doivent
-            être facilement accessible et compréhensible par tous, pour mieux
-            comprendre
-            <b
-              >qui, en France, crée la richesse, paye bien ses salarié⋅es, paye
-              des impôts</b
-            >, et d'autres question de ce genre que plein de citoyen⋅nes et
-            consommateur/rices français⋅ses se posent.
-          </p>
-        </div>
-      </div>
-      <div class="column is-half">
-        <img src="../assets/img/home/CA_Graphic_example.png" alt="" />
-      </div>
-    </div>
+    <TitleSection />
+    <PresentationSection />
     <div class="columns p-20 m-b-30 m-t-30 has-background-white-bis">
       <div class="column is-12">
         <div class="content">
@@ -104,14 +57,16 @@
 </template>
 
 <script>
-import SearchBar from "@/components/search/SearchBar";
 import PartnersSection from "@/components/home/PartnersSection";
+import TitleSection from "@/components/home/TitleSection";
+import PresentationSection from "@/components/home/PresentationSection";
 
 export default {
   name: "Home",
   components: {
-    SearchBar,
     PartnersSection,
+    PresentationSection,
+    TitleSection,
   },
 };
 </script>
