584caafdbfa4a814fb380edf2c6d69523a3a407c
amandine arnould <amandine.arnould90@gmail.com>
2021-09-16 15:28:06 +0200

    Commented navbar.scss unused code, removed css from NavBar.vue, added corresponding css in navbar.scss

--- src/components/site/NavBar.vue
+++ src/components/site/NavBar.vue
@@ -9,29 +9,3 @@
 <script>
 export default {};
 </script>
-
-<style lang="scss" scoped>
-.navbar-layout {
-  border-bottom: solid 1px darkgray;
-}
-
-.nav {
-  text-align: center;
-  padding: 10px;
-}
-
-.nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-.nav a.router-link-exact-active {
-  color: #2c3e5085;
-}
-
-.logo-enthic {
-  margin: 0.3rem 0 0 1rem;
-  width: 60px;
-  // transform: scale(0.2);
-}
-</style>
