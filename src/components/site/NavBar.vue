f40fe8650e1466ffb3feef175f5dbe81f5d7d2d1
amandine arnould <amandine.arnould90@gmail.com>
2021-09-16 16:45:56 +0200

    Navbar - Customized navbar design

--- src/components/site/NavBar.vue
+++ src/components/site/NavBar.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="navbar-layout">
-    <router-link to="/"
-      ><img src="@/assets/enthic.png" alt="Main logo" class="logo-enthic" />
+  <div class="navbar is-transparent navbar-layout">
+    <router-link to="/">
+      <img
+        src="@/assets/enthic_without_bg.png"
+        alt="Main logo"
+        class="logo-enthic"
+      />
     </router-link>
     <div class="navbar-item has-dropdown is-hoverable">
-      <a class="navbar-link is-arrowless"><i class="fas fa-bars"></i></a>
-      <div class="navbar-dropdown is-right">
+      <a class="navbar-link is-arrowless burger-menu">
+        <i class="fas fa-bars"></i>
+      </a>
+      <div class="navbar-dropdown is-right is-boxed">
         <a class="navbar-item">A propos</a>
         <a class="navbar-item">L'écosystème</a>
         <a class="navbar-item">Qui sommes-nous ?</a>
