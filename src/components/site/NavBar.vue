a57f125ba9891ed73a1e40783c327970488f7d1e
amandine arnould <amandine.arnould90@gmail.com>
2021-09-16 16:04:23 +0200

    Navbar - Added burger menu with submenus and removed navbar border

--- src/components/site/NavBar.vue
+++ src/components/site/NavBar.vue
@@ -3,6 +3,16 @@
     <router-link to="/"
       ><img src="@/assets/enthic.png" alt="Main logo" class="logo-enthic" />
     </router-link>
+    <div class="navbar-item has-dropdown is-hoverable">
+      <a class="navbar-link is-arrowless"><i class="fas fa-bars"></i></a>
+      <div class="navbar-dropdown is-right">
+        <a class="navbar-item">A propos</a>
+        <a class="navbar-item">L'écosystème</a>
+        <a class="navbar-item">Qui sommes-nous ?</a>
+        <hr class="navbar-divider" />
+        <div class="navbar-item">Version 2.0.3</div>
+      </div>
+    </div>
   </div>
 </template>
 
