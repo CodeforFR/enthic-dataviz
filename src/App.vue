8d7daeade1b091c16abf3a64860fcc05b28ce0f8
amandine arnould <amandine.arnould90@gmail.com>
2021-09-16 17:47:16 +0200

    Changed font family

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
 @import "@/assets/css/main.scss";
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
