99357883a7bb5091440af10f8d62b90cf72d6e11
amandine arnould <amandine.arnould90@gmail.com>
2021-09-17 15:26:38 +0200

    Added triangular shape divider

--- src/components/home/TitleSection.vue
+++ src/components/home/TitleSection.vue
@@ -15,7 +15,16 @@
         />
       </div>
     </div>
-
+    <div class="custom-shape-divider-top-1631883689">
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+      >
+        <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
+      </svg>
+    </div>
     <SearchBar />
     <div class="m-t-20">
       par exemple l'entreprise
