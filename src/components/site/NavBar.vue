fb1d9deb4b7cc5b9f6bfeacce5b162ca7b986c01
amandine arnould <amandine.arnould90@gmail.com>
2021-09-21 19:05:23 +0200

    Navbar - Rendered responsive the navbar

--- src/components/site/NavBar.vue
+++ src/components/site/NavBar.vue
@@ -1,27 +1,62 @@
 <template>
-  <div class="navbar is-transparent navbar-layout">
-    <router-link to="/">
-      <img
-        src="@/assets/enthic_without_bg.png"
-        alt="Main logo"
-        class="logo-enthic"
-      />
-    </router-link>
-    <div class="navbar-item has-dropdown is-hoverable">
-      <a class="navbar-link is-arrowless burger-menu">
-        <i class="fas fa-bars"></i>
+  <nav
+    class="navbar is-transparent navbar-layout"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <router-link to="/">
+        <img
+          src="@/assets/enthic_without_bg.png"
+          alt="Main logo"
+          class="logo-enthic"
+        />
+      </router-link>
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navMenu"
+      >
+        <i class="fas fa-bars burger-size"></i>
       </a>
-      <div class="navbar-dropdown is-right is-boxed">
+    </div>
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-end">
         <a class="navbar-item">A propos</a>
         <a class="navbar-item">L'écosystème</a>
         <a class="navbar-item">Qui sommes-nous ?</a>
-        <hr class="navbar-divider" />
-        <div class="navbar-item">Version 2.0.3</div>
       </div>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script>
 export default {};
+</script>
+
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
 </script>
