7f1e308247bcc2f30b275b42aa0654edefbee37f
amandine arnould <amandine.arnould90@gmail.com>
2021-10-13 19:23:02 +0200

    Search bar - Added responsiveness for all types of screen

--- src/components/search/SearchBar.vue
+++ src/components/search/SearchBar.vue
@@ -1,35 +1,39 @@
 <template>
-  <section id="top" class="container search-text primary-bg m-t-30">
-    <div class="background columns is-centered top-section">
-      <div class="column is-8">
-        <form action="/recherche" method="GET" class="columns search-bar">
-          <div class="column is-9">
-            <div class="control has-icons-left is-large">
+  <section id="top" class="search-text primary-bg m-t-30">
+    <!-- <div class="background columns is-centered top-section">
+      <div class="column is-8"> -->
+        <form action="/recherche" method="GET" class="search-bar">
+          <!-- <div class="column is-9"> -->
+            <div class="control has-icons-left">
               <input
                 type="search"
                 name="text"
-                class="input is-large search-box"
+                class="input search-box search-laptop"
                 :value="searchText"
                 placeholder="Saisissez le nom d'une entreprise (ex: enercoop)"
+              /><input
+                type="search"
+                name="text"
+                class="input search-box search-mobile"
+                :value="searchText"
+                placeholder="Nom de l'entreprise"
               />
               <span class="icon is-left" id="search-icon">
                 <i class="fas fa-search"></i>
               </span>
             </div>
-          </div>
-          <div class="column is-3 is-hidden-mobile has-text-centered">
+          <!-- </div> -->
+          <!-- <div class="has-text-centered"> -->
             <button class="search-btn" type="submit">
-              <span class="is-hidden-touch search-btn-as-text">
-                C'est parti 🚀
-              </span>
+              <span class="search-btn-as-text">C'est parti 🚀</span>
               <!-- <span class="icon is-hidden-desktop search-btn-as-icon">
                 <i class="fas fa-search"></i>
               </span> -->
             </button>
-          </div>
+          <!-- </div> -->
         </form>
-      </div>
-    </div>
+      <!-- </div>
+    </div> -->
   </section>
 </template>
 
