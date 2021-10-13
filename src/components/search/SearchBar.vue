52fcd325b8feb66612fa5b6d646549b1508c2ab8
amandine arnould <amandine.arnould90@gmail.com>
2021-10-13 19:39:45 +0200

    Fixed a bug on the banner width

--- src/components/search/SearchBar.vue
+++ src/components/search/SearchBar.vue
@@ -1,39 +1,28 @@
 <template>
   <section id="top" class="search-text primary-bg m-t-30">
-    <!-- <div class="background columns is-centered top-section">
-      <div class="column is-8"> -->
-        <form action="/recherche" method="GET" class="search-bar">
-          <!-- <div class="column is-9"> -->
-            <div class="control has-icons-left">
-              <input
-                type="search"
-                name="text"
-                class="input search-box search-laptop"
-                :value="searchText"
-                placeholder="Saisissez le nom d'une entreprise (ex: enercoop)"
-              /><input
-                type="search"
-                name="text"
-                class="input search-box search-mobile"
-                :value="searchText"
-                placeholder="Nom de l'entreprise"
-              />
-              <span class="icon is-left" id="search-icon">
-                <i class="fas fa-search"></i>
-              </span>
-            </div>
-          <!-- </div> -->
-          <!-- <div class="has-text-centered"> -->
-            <button class="search-btn" type="submit">
-              <span class="search-btn-as-text">C'est parti 🚀</span>
-              <!-- <span class="icon is-hidden-desktop search-btn-as-icon">
-                <i class="fas fa-search"></i>
-              </span> -->
-            </button>
-          <!-- </div> -->
-        </form>
-      <!-- </div>
-    </div> -->
+    <form action="/recherche" method="GET" class="search-bar">
+      <div class="control has-icons-left">
+        <input
+          type="search"
+          name="text"
+          class="input search-box search-laptop"
+          :value="searchText"
+          placeholder="Saisissez le nom d'une entreprise (ex: enercoop)"
+        /><input
+          type="search"
+          name="text"
+          class="input search-box search-mobile"
+          :value="searchText"
+          placeholder="Nom de l'entreprise"
+        />
+        <span class="icon is-left" id="search-icon">
+          <i class="fas fa-search"></i>
+        </span>
+      </div>
+      <button class="search-btn" type="submit">
+        <span class="search-btn-as-text">C'est parti 🚀</span>
+      </button>
+    </form>
   </section>
 </template>
 
