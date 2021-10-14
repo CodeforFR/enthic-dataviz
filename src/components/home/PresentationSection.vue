d190443e27702c3893413e6f0b1560cef794a1ae
amandine arnould <amandine.arnould90@gmail.com>
2021-10-14 14:20:49 +0200

    C'est quoi - Removed some text and graph

--- src/components/home/PresentationSection.vue
+++ src/components/home/PresentationSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="columns p-20 m-b-10 has-background-white-bis is-vcentered">
+  <!-- <div class="columns p-20 m-b-10 has-background-white-bis is-vcentered">
     <div class="column is-half">
       <div class="content">
         <h1 class="is-size-1">Enthic Dataviz : c'est quoi?</h1>
@@ -31,5 +31,17 @@
     <div class="column is-half">
       <img src="../../assets/img/home/CA_Graphic_example.png" alt="" />
     </div>
+  </div> -->
+
+  <div class="">
+    <h1>Enthic Dataviz : c'est quoi?</h1>
+    <p>
+      Enthic DataViz est un outil de data visualisation de la base de
+      données <a href="https://www.enthic.fr">Enthic</a> qui contient des
+      données sur plus de 1,6 millions d'entreprises françaises : bénéfice,
+      chiffre d'affaires, salaire, impôt, et plein d'autres informations que
+      seuls les comptables comprennent (tous les détails d'un compte de
+      résultat en fait).
+    </p>
   </div>
 </template>
