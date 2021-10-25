33ac2f0c1cd89ba22290744c37341984f9909f72
amandine arnould <amandine.arnould90@gmail.com>
2021-10-25 12:30:57 +0200

    Partner - Changed design and updated latitudes logo

--- src/components/home/PartnersSection.vue
+++ src/components/home/PartnersSection.vue
@@ -10,38 +10,47 @@
         <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
       </svg>
     </div>
-    <h1>L'écosystème</h1>
-    <a href="https://www.enthic.fr"
-      ><img src="/favicon.ico" alt="logo Enthic" class="is-128"
-    /></a>
+    <div class="partner-bckg">
+      <h1 class="partner-title">L'écosystème</h1>
 
-    <a href="https://codefor.fr/"
-      ><img
-        src="https://fork.osp.cat/uploads/decidim/organization/logo/1/medium_logo-code-for-france-alpha-gray.png"
-        alt="logo Code For France"
-        class="is-128"
-    /></a>
+      <div class="logo-container">
+        <a href="https://www.enthic.fr">
+          <img src="/favicon.ico" alt="logo Enthic" class="is-128 logo" />
+        </a>
 
-    <a href="https://wexample.com/"
-      ><img
-        src="https://wexample.com/wp-content/uploads/2021/04/cropped-favicon-wexample-ss-bk-192x192.png"
-        alt="Logo Wexample"
-        class="is-128"
-    /></a>
+        <a href="https://codefor.fr/">
+          <img
+            src="https://fork.osp.cat/uploads/decidim/organization/logo/1/medium_logo-code-for-france-alpha-gray.png"
+            alt="logo Code For France"
+            class="is-128 logo"
+          />
+        </a>
 
-    <a href="https://dataforgood.fr"
-      ><img
-        src="https://dataforgood.fr/img/logo-white.png"
-        class="is-128"
-        alt="logo Data For Good"
-    /></a>
+        <a href="https://wexample.com/">
+          <img
+            src="https://wexample.com/wp-content/uploads/2021/04/cropped-favicon-wexample-ss-bk-192x192.png"
+            alt="Logo Wexample"
+            class="is-128 logo"
+          />
+        </a>
 
-    <a href="https://www.latitudes.cc/"
-      ><img
-        src="//user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/1127947/550388_944467.png"
-        alt="logo Latitude - Exploring Tech for Good"
-        class="is-128"
-    /></a>
+        <a href="https://dataforgood.fr">
+          <img
+            src="https://dataforgood.fr/img/logo-white.png"
+            class="is-128 logo"
+            alt="logo Data For Good"
+          />
+        </a>
+
+        <a href="https://www.latitudes.cc/">
+          <img
+            src="https://uploads-ssl.webflow.com/610e20d69f9e1d3cae586963/6149d32f1c27af536f04ba69_Logo_Latitudes.svg"
+            alt="logo Latitude - Exploring Tech for Good"
+            class="is-128 logo"
+          />
+        </a>
+      </div>
+    </div>
   </section>
 </template>
 
