59c01cd3a533c3e6cd3d78ac9ccee0867dc5e8a8
amandine arnould <amandine.arnould90@gmail.com>
2021-10-26 15:38:03 +0200

    Changed footer design

--- src/components/site/Footer.vue
+++ src/components/site/Footer.vue
@@ -1,62 +1,53 @@
 <template>
-  <footer class="footer">
-    <div class="container">
-      <div class="columns">
-        <!-- LOOP FOOTER BLOCKS -->
-        <div class="column is-5 is-offset-1">
-          <!-- BLOCK TITLE -->
-          <h3 class="is-3">Aller plus loin</h3>
-          <ul>
-            <li>
-              <a href="https://www.enthic.fr/"
-                >La base de données Enthic.fr utilisée ici</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://www.inpi.fr/fr/licence-registre-national-du-commerce-et-des-societes-rncs"
-                >La source officielle des données sur l'INPI</a
-              >
-            </li>
-            <li><a href=""></a></li>
-          </ul>
-        </div>
-
-        <div class="column is-5 is-offset-1">
-          <!-- BLOCK TITLE -->
-          <h3 class="is-3">Participer</h3>
-          <ul>
-            <li>
-              <a href="https://chat.codefor.fr"
-                >Le chat pour venir nous parler</a
-              >
-            </li>
-            <li>
-              <a href="https://github.com/phe-sto/enthic"
-                >Code source du back</a
-              >
-            </li>
-            <li>
-              <a href="https://github.com/CodeforFR/enthic-dataviz"
-                >Code source du front</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
+  <footer class="top-footer">
+    <div class="more">
+      <h3 class="is-3">Aller plus loin</h3>
+      <ul>
+        <li>
+          <a href="https://www.enthic.fr/" target="_blank">Base de données</a>
+        </li>
+        <li>
+          <a
+            href="https://www.inpi.fr/fr/licence-registre-national-du-commerce-et-des-societes-rncs"
+            target="_blank"
+            >Source des données</a
+          >
+        </li>
+        <li><a href=""></a></li>
+      </ul>
+    </div>
+    <div class="icons">
+      <a
+        href="https://data-for-good.slack.com/archives/C01AQR42BUN"
+        target="_blank"
+        class="icon-size"
+      >
+        <i class="fab fa-slack"></i>
+      </a>
+      <a
+        href="https://github.com/CodeforFR/enthic-dataviz"
+        class="icon-size"
+        target="_blank"
+      >
+        <i class="fab fa-github-alt"></i>
+      </a>
+    </div>
+    <div class="code">
+      <h3 class="is-3">Participer</h3>
+      <ul>
+        <li>
+          <a href="https://chat.codefor.fr" target="_blank">Chat</a>
+        </li>
+        <li>
+          <a href="https://github.com/phe-sto/enthic" target="_blank">
+            Code source back
+          </a>
+          /
+          <a href="https://github.com/CodeforFR/enthic-dataviz" target="_blank">
+            front
+          </a>
+        </li>
+      </ul>
     </div>
   </footer>
 </template>
-
-<style lang="scss" scoped>
-.enthic-name {
-  text-decoration: underline;
-  color: black;
-  font-weight: bold;
-}
-
-.layout {
-  padding: 1rem 0;
-  text-align: center;
-}
-</style>
